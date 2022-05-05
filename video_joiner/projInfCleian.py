384461033fcc73e2be9051880c1ad33402758614
Joao <jmvs98@gmail.com>
2022-05-05 14:20:28 +0100

    Function to join videos + Python script clean (without comments)

--- video_joiner/projInfCleian.py
+++ video_joiner/projInfCleian.py
@@ -1,4 +1,4 @@
-import os #para mudar a diretoria de trabalho
+import os
 import sys, getopt
 import ffmpeg
 from pathlib import Path
@@ -22,36 +22,22 @@ def main(argv):
         elif opt in ("-p", "--path"):
             folder_path = arg
 
-    #Guarda a diretoria onde o ficheiro .py está a ser executado
+
     working_dir = os.getcwd()
 
 
-    #https://stackoverflow.com/questions/973473/getting-a-list-of-all-subdirectories-in-the-current-directory
-    #Função retirada do stackoverflow, que retorna todas as diretorias e subdiretorias da pasta selecionada
-    #A função é recursiva, em que vai sempre ser chamada até sejam listadas todas as subdiretorias de uma diretoria
-    #Todas estas diretorias são adicionadas à lista subfolders[]
     def get_all_dirs(dirname):
         subfolders = [f.path for f in os.scandir(dirname) if f.is_dir()]
         for dirname in list(subfolders):
             subfolders.extend(get_all_dirs(dirname))
         return subfolders
 
-    #Função que retorna todos os ficheiros .mp4 encontrados nas diretorias e subdiretorias da pasta selecionada
+
     def get_all_mp4_files_in_dirs(all_dirs):
-        #Incialização da lista
         all_mp4_files = []
-
-        #Percorrer todas as diretorias e subdiretorias retornadas pela função get_all_dirs()
         for i in all_dirs:
-
-            #Lista os ficheiros contidos na diretoria que está a analizar
             files_in_dir=os.listdir(i)
-
-            #Percorre cada ficheiro da diretoria
             for u in files_in_dir:
-
-                #Verifica se a extenção do ficheiro é .mp4
-                #Caso seja, adiciona à lista all_mp4_files[] com o caminho ("path") completo do ficheiro
                 filename, file_extension = os.path.splitext(u)
                 if file_extension.lower() == '.mp4':
                     all_mp4_files.append(i+"\\"+filename+file_extension)
@@ -64,37 +50,25 @@ def main(argv):
                 f.write("file '"+video+"'\n")
 
 
-        #https://stackoverflow.com/questions/17668996/python-how-to-join-multiple-video-files
-        # set output filename
         outFile = Path(working_dir+'\\final.mp4')
 
-        # create file object that will contain files for ffmpeg to concat
         input_files_path = Path(working_dir+'\\temp_videos_paths.txt')
 
-        # this seems to be the proper concat input, with the path containing the list
-        # of files for ffmpeg to concat, along with the format parameter, and safe, if i
-        # read the docs correctly, is default/optional
         ffInput = ffmpeg.input(input_files_path, format='concat', safe=0)
 
-        # output parameters
         params =    {
                     'c:a': 'aac'
                     }
         
-        # input stream -> output stream with output filename and expanded params
         ffOutput = ffInput.output(outFile.as_posix(), **params)
 
-        # something, something, run.
         ffOutput.run(overwrite_output=True)
 
-        #Removing temp file of videos paths
         os.remove(input_files_path)
 
-    #Executar as funções para obter todos os ficheiros .mp4
     all_folders = get_all_dirs(folder_path)
     all_files_in_folders = get_all_mp4_files_in_dirs(all_folders)
 
-    #Executar a função para juntar os vídeos
     ffmpeg_joiner(all_files_in_folders)
 
 
